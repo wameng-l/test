1d2d973b7ed9590525d50337746f865ef3dc5aaa
wameng-l <wamenglobriayao@gmail.com>
2024-09-20 17:01:43 +0700

    [update] more home

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -6,6 +6,16 @@ export default function Home() {
    Home 3
    Home 4
    Home 5
+   Home 1
+   Home 2
+   Home 3
+   Home 4
+   Home 5
+   Home 1
+   Home 2
+   Home 3
+   Home 4
+   Home 5
   </>
   );
 }
