c98b7eb780bbebd3a97a04e9f30b5677d2497dc8
wameng-l <wamenglobriayao@gmail.com>
2024-09-20 16:11:58 +0700

    Create Navbar.tsx

---
+++ next/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        Logo
+      </div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
